3b19121a5129a559c188f16895070dedc31d6855
SaloniPundir <salonipn09@gmail.com>
2025-01-08 17:31:15 +0530

    -added tailwindCss

--- task-frontend/src/App.js
+++ task-frontend/src/App.js
@@ -1,7 +1,31 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Login from "./components/forms/login";
+import SignUp from "./components/forms/signUp";
+import Home from "./components/pages/home";
+import Task from "./components/pages/task";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <><Home/></>
+    },
+    {
+      path: "/login",
+      element: <><Login/></>
+    },
+    {
+      path: "/signUp",
+      element: <><SignUp/></>
+    },
+    {
+      path: "/task",
+      element: <><Task/></>
+    },
+  ])
   return (
-    <div className="App">
-      
+    <div>
+      <RouterProvider router={router}/>
     </div>
   );
 }
---
+++ task-frontend/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", "components/**/*.{js,jsx}"
+  ],
+  theme: {
+    screens: {
+      sm: "340px", // mobile 
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px", // tablet
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px", // laptop
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px", // desktop
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px", // large desktop
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
